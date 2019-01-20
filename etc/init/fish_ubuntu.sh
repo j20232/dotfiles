bd8c197741f5b60e32f3b7f523d587aa74b1c778
mocobt <mocobt@gmail.com>
2019-01-20 22:42:03 +0900

    Updated fish_ubuntu.sh

--- etc/init/fish_ubuntu.sh
+++ etc/init/fish_ubuntu.sh
@@ -19,6 +19,7 @@ source $XDG_CONFIG_HOME/fish/config.fish
 pyenv install 3.6.6
 pyenv global 3.6.6
 sudo apt install python-pip -y
+sudo update-locale LC_ALL=en_US.UTF-8
 
 ln -s $HOME/dotfiles/nvim/ $XDG_CONFIG_HOME/
 sudo apt-add-repository ppa:neovim-ppa/stable
@@ -30,8 +31,8 @@ pip install kaggle
 pip install numpy pandas matplotlib seaborn scikit-learn lightgbm joblib pathlib numba fasteners
 pip install "dask[complete]"
 
-echo "give me e-mail"
-read EMAIL
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
-chmod 600 $HOME/.ssh/id_rsa
+# echo "give me e-mail"
+# read EMAIL
+# ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
+# chmod 600 $HOME/.ssh/id_rsa
 eval (pyenv init - | source)
