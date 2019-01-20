642277a7799d06e7e44dccfbedad224f56f3abce
mocobt <mocobt@gmail.com>
2019-01-20 17:45:09 +0900

    Revised fish_init.sh

--- etc/init/fish_init.sh
+++ etc/init/fish_init.sh
@@ -7,55 +7,53 @@ source $XDG_CONFIG_HOME/fish/config.fish
 set -eu
 
 # by OS
-case ${OSTYPE} in
-    darwin*)
-        echo "Running on OSX"
-        # source install/brew.sh
-        ;;
-    linux*)
-        if   [ -e /etc/debian_version ] ||
-                 [ -e /etc/debian_release ]; then
-            # Check Ubuntu or Debian
-            if [ -e /etc/lsb-release ]; then
-                # Ubuntu
-                # distri_name="ubuntu"
-                echo "Running on ubuntu"
-                source etc/init/fish_ubuntu.sh
-            else
-                # Debian
-                distri_name="debian"
-            fi
-        elif [ -e /etc/fedora-release ]; then
-            # Fedra
-            distri_name="fedora"
-        elif [ -e /etc/redhat-release ]; then
-            if [ -e /etc/oracle-release ]; then
-                # Oracle Linux
-                distri_name="oracle"
-            else
-                # Red Hat Enterprise Linux
-                # CentOS
-                distri_name="redhat"
-            fi
-        elif [ -e /etc/turbolinux-release ]; then
-            # Turbolinux
-            distri_name="turbol"
-        elif [ -e /etc/SuSE-release ]; then
-            # SuSE Linux
-            distri_name="suse"
-        elif [ -e /etc/mandriva-release ]; then
-            # Mandriva Linux
-            distri_name="mandriva"
-        elif [ -e /etc/vine-release ]; then
-            # Vine Linux
-            distri_name="vine"
-        elif [ -e /etc/gentoo-release ]; then
-            # Gentoo Linux
-            distri_name="gentoo"
-        else
-            # Other
-            echo "unkown distribution"
-            distri_name="unkown"
-        fi
-        ;;
-esac
+switch(uname)
+  case Darwin
+    echo "Running on OSX"
+  case Linux
+    echo "Running on Linux"
+    if test -e /etc/debian_version; or test -e /etc/debian_release
+      # Check Ubuntu or Debian
+      if test -e /etc/lsb-release
+        # Ubuntu
+        echo "Running on Ubuntu"
+        source etc/init/fish_ubuntu.sh
+      else
+        # Debian
+        distri_name="debian"
+      end
+    else if test -e /etc/fedora-release
+      # Fedra
+      distri_name="fedora"
+    else if test -e /etc/redhat-release
+      if test -e /etc/oracle-release
+        # Oracle Linux
+        distri_name="oracle"
+      else
+        # Red Hat Enterprise Linux
+        # CentOS
+        distri_name="redhat"
+      end
+    else if test -e /etc/turbolinux-release
+      # Turbolinux
+      distri_name="trubol"
+    else if test -e /etc/SuSe-release
+      # SuSE Linux
+      distri_name="suse"
+    else if test -e /etc/mandriva-release
+      # Mandriva Linux
+      distri_name="mandriva"
+    else if test -e /etc/vine-release
+      # Vine Linux
+      distri_name="vine"
+    else if test -e /etc/gentoo-release
+      # Gentoo Linux
+      distri_name="gentoo"
+    else
+      # Other
+      echo "Unknown distribution"
+      distri_name="unknown"
+    end
+  end
+
+                 
