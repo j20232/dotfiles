9c5705798898502f9756a76b813e0635d488b787
mocobt <mocobt@gmail.com>
2019-01-20 23:48:25 +0900

    Updated bash_ubuntu.sh

--- etc/init/fish_ubuntu.sh
+++ etc/init/fish_ubuntu.sh
@@ -4,15 +4,6 @@ XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
 
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher add hauleth/agnoster
-fisher add oh-my-fish/plugin-extract
-git clone https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
-fisher add jethrokuan/fzf
-fisher add jethrokuan/z
-fisher add 0rax/fish-bd
-
 sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 source $XDG_CONFIG_HOME/fish/config.fish
@@ -24,6 +15,7 @@ ln -s $HOME/dotfiles/nvim/ $XDG_CONFIG_HOME/
 sudo apt-add-repository ppa:neovim-ppa/stable
 sudo apt update
 yes | sudo apt install neovim
+eval (pyenv init - | source)
 pip install neovim flake8
 pip install jupyter jupyter_contrib_nbextensions jupyterthemes
 pip install kaggle
@@ -35,3 +27,14 @@ eval (pyenv init - | source)
 # read EMAIL
 # ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
 # chmod 600 $HOME/.ssh/id_rsa
+
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+fisher add hauleth/agnoster
+fisher add oh-my-fish/plugin-extract
+git clone https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+fisher add jethrokuan/fzf
+fisher add jethrokuan/z
+fisher add 0rax/fish-bd
+
+eval (pyenv init - | source)
