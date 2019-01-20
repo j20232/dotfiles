2ba4f46e51cc6afb3910f052dcdcf30ed8c03218
mocobt <mocobt@gmail.com>
2019-01-20 16:07:37 +0900

    a

--- etc/init/fish_ubuntu.sh
+++ etc/init/fish_ubuntu.sh
@@ -14,28 +14,28 @@ fisher add jethrokuan/fzf
 fisher add jethrokuan/z
 fisher add 0rax/fish-bd
 
-sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
-source $XDG_CONFIG_HOME/fish/config.fish
-pyenv install 3.6.6
-pyenv global 3.6.6
-pip install pip==18.0
+# sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
+# source $XDG_CONFIG_HOME/fish/config.fish
+# pyenv install 3.6.6
+# pyenv global 3.6.6
+# pip install pip==18.0
 
-mkdir $XDG_CONFIG_HOME/nvim
-ln -s $HOME/dotfiles/nvim/ $XDG_CONFIG_HOME/
-sudo apt-add-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y neovim
-pip install neovim
-pip install flake8
+# mkdir $XDG_CONFIG_HOME/nvim
+# ln -s $HOME/dotfiles/nvim/ $XDG_CONFIG_HOME/
+# sudo apt-add-repository ppa:neovim-ppa/stable
+# sudo apt update
+# sudo apt install -y neovim
+# pip install neovim
+# pip install flake8
 
-pip install jupyter
-pip install jupyter_contrib_nbextensions
-pip install jupyterthemes
+# pip install jupyter
+# pip install jupyter_contrib_nbextensions
+# pip install jupyterthemes
 
-pip install kaggle
+# pip install kaggle
 
-echo "give me e-mail"
-read EMAIL
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
-chmod 600 $HOME/.ssh/id_rsa
+# echo "give me e-mail"
+# read EMAIL
+# ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
+# chmod 600 $HOME/.ssh/id_rsa
 
