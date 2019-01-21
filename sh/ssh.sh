06046f83b3516f78bdaf153fcd2eb60d938e2b10
mocobt <mocobt@gmail.com>
2019-01-21 23:19:58 +0900

    Update ssh.sh

--- sh/ssh.sh
+++ sh/ssh.sh
@@ -1,4 +1,3 @@
 echo "give me e-mail"
-read EMAIL
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
+read EMAIL; ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
 chmod 600 $HOME/.ssh/id_rsa
