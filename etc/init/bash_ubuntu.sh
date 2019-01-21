32626b6015d4643087fccd772bd3e2a7ced4e952
mocobt <mocobt@gmail.com>
2019-01-21 22:54:00 +0900

    Update bash_ubuntu.sh

--- etc/init/bash_ubuntu.sh
+++ etc/init/bash_ubuntu.sh
@@ -1,6 +1,5 @@
-# XDG Base Directory
-# https://wiki.archlinux.org/index.php/XDG_Base_Directory
 echo "Setting up XDG Base Directory..."
+echo "Detail of XDG Base Directory is here: https://wiki.archlinux.org/index.php/XDG_Base_Directory"
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
