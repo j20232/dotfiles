cf0b5c7bee4bc1b30c5115ac7549e7b0fb77eb93
mocobt <mocobt@gmail.com>
2019-01-21 22:57:04 +0900

    Update fish_ubuntu.sh

--- etc/init/fish_ubuntu.sh
+++ etc/init/fish_ubuntu.sh
@@ -1,13 +1,13 @@
-# XDG Base Directory
-# https://wiki.archlinux.org/index.php/XDG_Base_Directory
-XDG_CONFIG_HOME=$HOME/.config
-XDG_CACHE_HOME=$HOME/.cache
-XDG_DATA_HOME=$HOME/.local/share
+echo "Setting up PATH..."
+set XDG_CONFIG_HOME $HOME/.config $XDG_CONFIG_HOME
+set XDG_CACHE_HOME $HOME/.cache $XDG_CACHE_HOME
+set XDG_DATA_HOME $HOME/.local/share $XDG_DATA_HOME
+source $XDG_CONFIG_HOME/fish/config.fish
 
+echo "Setting up pyenv..."
 sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 source $XDG_CONFIG_HOME/fish/config.fish
 pyenv install 3.6.6
 pyenv global 3.6.6
 sudo apt install python-pip -y
-
