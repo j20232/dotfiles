bf49098066d6a9ebc3acf49ec70ac5b46d28aa33
mocobt <mocobt@gmail.com>
2019-01-21 22:14:23 +0900

    Update bash_ubuntu.sh

--- etc/init/bash_ubuntu.sh
+++ etc/init/bash_ubuntu.sh
@@ -3,6 +3,7 @@
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
+sudo chown $USER -R $HOME
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
 
 # apt install
@@ -19,8 +20,7 @@ sudo /usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG
 ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/
 
 # fish
-sudo apt-add-repository ppa:fish-shell/release-2
+yes | sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
-sudo apt-get install -y fish
-sudo chown $USER -R $HOME
+sudo apt-get install fish
 sudo chsh $USER -s $(which fish)
