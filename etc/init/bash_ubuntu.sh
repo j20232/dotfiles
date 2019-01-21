9b9247347579ed1441f41c6015c8c59725d7aab1
mocobt <mocobt@gmail.com>
2019-01-21 22:53:19 +0900

    Update bash_ubuntu.sh

--- etc/init/bash_ubuntu.sh
+++ etc/init/bash_ubuntu.sh
@@ -1,14 +1,17 @@
 # XDG Base Directory
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory
+echo "Setting up XDG Base Directory..."
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
 sudo chown $USER -R $HOME
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
 
-# apt install
+echo "Installing minimum packages..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y tmux htop tree wget curl cmake gcc g++ build-essential ca-certificates software-properties-common
+
+echo "Setting up the locale..."
 sudo update-locale LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=$LANG
@@ -17,6 +20,7 @@ sudo dpkg-reconfigure -f noninteractive locales
 sudo /usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG
 
 # fish
+echo "Install fish..."
 yes | sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install -y fish
