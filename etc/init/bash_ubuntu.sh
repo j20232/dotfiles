dfe8c87a81a47a7de05e1e90d58ee4efe20876fb
mocobt <mocobt@gmail.com>
2019-01-21 21:59:22 +0900

    Update bash_ubuntu.sh

--- etc/init/bash_ubuntu.sh
+++ etc/init/bash_ubuntu.sh
@@ -16,12 +16,11 @@ sudo dpkg-reconfigure -f noninteractive locales
 sudo /usr/sbin/update-locale LANG=$LANG LC_ALL=$LANG
 
 # tmux
-# ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/
+ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/
 
 # fish
 yes | sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
-# yes | sudo apt-get install fish
-# sudo mkdir $XDG_CONFIG_HOME/fish
-# sudo chown $USER -R $HOME
-# sudo chsh $USER -s $(which fish)
+yes | sudo apt-get install fish
+sudo chown $USER -R $HOME
+sudo chsh $USER -s $(which fish)
