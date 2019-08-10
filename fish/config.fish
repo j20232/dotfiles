4a1976c4cc0d36afd08ad3c9b78864766997273b
mocobt <mocobt@gmail.com>
2019-08-10 22:27:22 +0900

    Add winresizer

--- fish/config.fish
+++ fish/config.fish
@@ -22,6 +22,7 @@ set PATH $PYENV_ROOT/bin $PATH
 set PATH /usr/local/bin $PATH
 set LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
 
+
 if not test -e $HOME/.tmux.conf
   ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/
 end
