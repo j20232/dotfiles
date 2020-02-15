87a38ee5b48e097f165f39a6a40f2553a745f942
mocobt <mocobt@gmail.com>
2020-02-16 03:50:24 +0900

    updated

--- python/nn.sh
+++
@@ -1,4 +0,0 @@
-pip install opencv-python
-pip install Pillow==6.2.0
-pip install torch torchvision
-pip install pretrainedmodels albumentations
--- python/table.sh
+++
@@ -1,2 +0,0 @@
-pip install lightgbm
-pip install "dask[complete]"
