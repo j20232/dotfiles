5a822f6bccf10c4eb36652952f16c0a31a7fefbf
mocobt <mocobt@gmail.com>
2020-02-16 04:08:56 +0900

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -19,19 +19,22 @@ set XDG_DATA_HOME $HOME/.local/share $XDG_DATA_HOME
 set PYENV_ROOT $HOME/.pyenv $PYENV_ROOT
 set PATH $PYENV_ROOT/bin $PATH
 set PATH /usr/local/bin $PATH
+set -x PIPENV_SKIP_LOCK true
 
+# C++
 switch (uname)
   case Darwin
     if not test -e /usr/local/bin/gcc
       ln -s /usr/local/bin/gcc-9 /usr/local/bin/gcc
       ln -s /usr/local/bin/g++-9 /usr/local/bin/g++
     end
+
+# CUDA
+switch (uname)
   case Linux
     set PATH /usr/local/cuda/bin $PATH
     set LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
   end
-
-set -x PIPENV_SKIP_LOCK true
 
 
 if not test -e $HOME/.tmux.conf
@@ -55,4 +58,14 @@ if test -e $XDG_CONFIG_HOME/local.fish
   source $XDG_CONFIG_HOME/local.fish
 end
 
-source $HOME/dotfiles/fish/pyenv.sh
+# pyenv
+switch (uname)
+  case Darwin
+    if test -e /usr/local/bin/pyenv  
+      eval (pyenv init - | source)
+    end
+  case Linux
+    if test -e $HOME/.pyenv/bin/pyenv
+      eval (pyenv init - | source)
+    end
+  end
