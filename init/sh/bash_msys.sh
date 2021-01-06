94dcd8d009039c23124e28796eded93ad510511e
mocobt <mocobt@gmail.com>
2021-01-06 18:15:28 +0900

    fix user

--- init/sh/bash_msys.sh
+++ init/sh/bash_msys.sh
@@ -2,7 +2,9 @@ echo "Setting up XDG Base Directory..."
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
-chown $USER -R $HOME
+if [-z "$USER"]; then
+    chown $USER -R $HOME
+fi
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
 
 echo "Installing minimum packages..."
