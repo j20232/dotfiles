464b9a970e0f2311ecd45585613ddc5ea186502b
mocobt <mocobt@gmail.com>
2021-01-06 18:26:43 +0900

    rm chown fr msys

--- init/sh/bash_msys.sh
+++ init/sh/bash_msys.sh
@@ -2,9 +2,6 @@ echo "Setting up XDG Base Directory..."
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
-if [-z "$USER"]; then
-    chown $USER -R $HOME
-fi
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
 
 echo "Installing minimum packages..."
