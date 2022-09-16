3ce1837e6c79800b0cc3273bbbd7ddfb78fc300a
mocobt <mocobt@gmail.com>
2022-09-16 17:48:01 +0900

    Update fish_init.sh

--- init/sh/fish_init.sh
+++ init/sh/fish_init.sh
@@ -29,7 +29,7 @@ fisher add jethrokuan/fzf
 fisher add edc/bass
 
 # gibo
-git clone git@github.com:simonwhitaker/gibo.git ~/gibo
+git clone https://github.com/simonwhitaker/gibo.git ~/gibo
 cd $HOME/gibo
 chmod +x ~/gibo
 set PATH $HOME/gibo $PATH
